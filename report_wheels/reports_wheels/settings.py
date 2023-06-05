df2ff5c7974ec37dab3d8a7df607b1323c440c41
Ahmed_Qureshi <aa10980@gmail.com>
2023-06-05 20:07:48 +0530

    fix attempt2

--- report_wheels/reports_wheels/settings.py
+++ report_wheels/reports_wheels/settings.py
@@ -25,26 +25,26 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-st_0bs7%t8mbai+l$_yeue1s_&u5-*e71b2%r)*mt#*g5oh8j#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
-DEBUG = False
+# DEBUG = False
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "172.20.100.81",
-    "http://localhost:8009",
-    "http://172.20.100.81:8009",
-]
+# ALLOWED_HOSTS = [
+#     "127.0.0.1",
+#     "localhost",
+#     "172.20.100.81",
+#     "http://localhost:8009",
+#     "http://172.20.100.81:8009",
+# ]
 
 
-CSRF_TRUSTED_ORIGINS = [
-    "127.0.0.1",
-    "localhost",
-    "172.20.100.81",
-    "http://localhost:8009",
-    "http://172.20.100.81:8009",
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     "127.0.0.1",
+#     "localhost",
+#     "172.20.100.81",
+#     "http://localhost:8009",
+#     "http://172.20.100.81:8009",
+# ]
 
 
 # Application definition
